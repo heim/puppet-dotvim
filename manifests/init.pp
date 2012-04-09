70aadb1460096737bfc76722e9b4172f600ce691
Andreas Heim <andreas@heim.no>
2012-04-10 00:39:13 +0200

    wrapped defines in a class, made sure Package git core was not defined twice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,36 @@
-define vim($git_repo="git://github.com/heim/dotvim.git") {
-  
-  Package["git-core"] -> Exec["clone-vim-files-for-$name"] -> File["/home/$name/.vimrc"]
+class dotvim {
+	define vim($git_repo="git://github.com/heim/dotvim.git") {
 
-  package { "git-core":
-    ensure => present,
-  }
+		Package["git-core"] -> Exec["clone-vim-files-for-$name"] -> File["/home/$name/.vimrc"]
 
-  exec { "clone-vim-files-for-$name":
-    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin",
-    user => "$name",
-    cwd => "/home/$name",
-    command => "git clone $git_repo .vim",
-    creates => "/home/$name/.vim",
-  }
+    if(!defined(Package["git-core"])) {
+      package { "git-core":
+        ensure => present,
+      }
+    }
+
+		exec { "clone-vim-files-for-$name":
+			path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin",
+			     user => "$name",
+			     cwd => "/home/$name",
+			     command => "git clone $git_repo .vim",
+			     creates => "/home/$name/.vim",
+		}
 
 
-  file { "/home/$name/.vimrc":
-    ensure => link,
-    target => "/home/$name/.vim/vimrc",
-    owner => "$name",
-  }
+		file { "/home/$name/.vimrc":
+			ensure => link,
+			       target => "/home/$name/.vim/vimrc",
+			       owner => "$name",
+		}
 
-  exec { "update-vim-files-for-$name":
-    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin",
-    user => "$name", 
-    cwd => "/home/$name/.vim",
-    command => "git pull origin master",
-    require => Exec["clone-vim-files-for-$name"],
-  }
+		exec { "update-vim-files-for-$name":
+			path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin",
+			     user => "$name", 
+			     cwd => "/home/$name/.vim",
+			     command => "git pull origin master",
+			     require => Exec["clone-vim-files-for-$name"],
+		}
 
+	}
 }
